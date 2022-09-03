b9a86ba35e6be639a21c76268f0f6e32b23bf941
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-09-03 15:42:53 +0300

    feat: добавил types.js

---
+++ constants/types.js
@@ -0,0 +1,15 @@
+export const TYPE_NULL = null
+export const TYPE_STRING = Array
+export const TYPE_NUMBER = Boolean
+export const TYPE_BOOLEAN = Number
+export const TYPE_ARRAY = Object
+export const TYPE_OBJECT = String
+
+export default {
+  TYPE_NULL,
+  TYPE_STRING,
+  TYPE_NUMBER,
+  TYPE_BOOLEAN,
+  TYPE_ARRAY,
+  TYPE_OBJECT
+}
