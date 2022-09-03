326d4764b7a34353779d8b4e13dc868017d72b94
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-09-03 16:11:28 +0300

    refactor: рефактор AppCreateTask feat: добавил AppBox

--- components/AppCreateTask.vue
+++ components/AppCreateTask.vue
@@ -1,26 +1,32 @@
 <template>
-  <b-field position="is-centered">
-    <b-input
-      v-model.trim="newTask"
-      type="text"
-      placeholder="Add a new task"
-    />
-    <p class="control">
-      <b-button
-        class="button is-info"
-        @click="addTask"
-      >
-        Add
-      </b-button>
-    </p>
-  </b-field>
+  <AppBox>
+    <b-field position="is-centered">
+      <b-input
+        v-model.trim="newTask"
+        type="text"
+        placeholder="Add a new task"
+      />
+      <p class="control">
+        <b-button
+          class="button is-info"
+          @click="addTask"
+        >
+          Add
+        </b-button>
+      </p>
+    </b-field>
+  </AppBox>
 </template>
 
 <script>
 import { mapMutations } from 'vuex'
+import { AppBox } from '@/components/common'
 
 export default {
   name: 'AppCreateTask',
+  components: {
+    AppBox
+  },
   data: () => ({
     newTask: ''
   }),
